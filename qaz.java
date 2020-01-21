6988fdb3c4c1c9d7c84a12772e1f60824ed38d13
YMC <a40104401@gmail.com>
2020-01-21 21:29:16 +0800

    123

--- qaz.java
+++ qaz.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-	 int asad789;
+	 int asad;
 	public static void main(String[] args){
             System.out.println("Hello World!");
 			System.out.println("Hello World2!");
